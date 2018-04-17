ebd3b7187bae107318c490fd042db96d25cfaf15
Yasu <yassantune@gmail.com>
2018-04-17 06:05:49 +0000

    final step

--- download_avatars.js
+++ download_avatars.js
@@ -4,32 +4,35 @@ var myAuth = require('./secrets.js');
 var owner = process.argv[2];
 var repo = process.argv[3];
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': 'token ' + myAuth.token
-    }
-  };
-  request(options, function(err, res, body) {
-    body = JSON.parse(body);
-    var output = '';
-    var path = ''
-    for ( var key in body) {
-      output = (body[key].avatar_url);
-      path = ('avatars/' + body[key].login + '.jpg');
-      // send path and login name, not using array.
-      cb(output, path);
-    }
-  });
+if (!owner || !repo) {
+  console.log('Please specify reposit owner and reposit name!\n e.g.: node download_avatars.js repoOwner repoName');
+} else {
+  function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': 'token ' + myAuth.token
+      }
+    };
+    request(options, function(err, res, body) {
+      body = JSON.parse(body);
+      var output = '';
+      var path = ''
+      for ( var key in body) {
+        output = (body[key].avatar_url);
+        path = ('avatars/' + body[key].login + '.jpg');
+        // send path and login name, not using array.
+        cb(output, path);
+      }
+    });
+  }
+
+  function downloadImageByURL(url, filePath) {
+    // get and write (file is named as filePath here.)
+    request.get(url).pipe(fs.createWriteStream(filePath));
+  }
+
+  getRepoContributors(owner, repo, downloadImageByURL);
+  console.log('Saved images!');
 }
-
-function downloadImageByURL(url, filePath) {
-  // get and write (file is named as filePath here.)
-  request.get(url).pipe(fs.createWriteStream(filePath));
-}
-
-getRepoContributors(owner, repo, downloadImageByURL);
-
-console.log('Saved images!');
