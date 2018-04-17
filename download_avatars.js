4c002e30b95d07b34a17c913739ee0a23a48761d
Yasu <yassantune@gmail.com>
2018-04-17 06:08:02 +0000

    code cleaned

--- download_avatars.js
+++ download_avatars.js
@@ -22,14 +22,13 @@ if (!owner || !repo) {
       for ( var key in body) {
         output = (body[key].avatar_url);
         path = ('avatars/' + body[key].login + '.jpg');
-        // send path and login name, not using array.
         cb(output, path);
       }
     });
   }
 
   function downloadImageByURL(url, filePath) {
-    // get and write (file is named as filePath here.)
+
     request.get(url).pipe(fs.createWriteStream(filePath));
   }
 
